6e66f6e37118366439cfb20614fda08926b1a274
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-18 15:58:03 +0100

    create the bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - function that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, start;
+	int tmp, swaped;
+
+	for (i = 0; i < size; i++)
+	{
+		swaped = 0;
+		for (j = 0; j + i < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swaped = 1;
+			}
+			print_array(array, size);
+		}
+
+		if (swaped == 0)
+			break;
+	}
+}
