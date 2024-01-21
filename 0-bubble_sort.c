7a81f68196af4b1c0da05af104e46cb5bd796b19
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-21 18:13:32 +0100

    make some modeffications

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,13 +10,12 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp, swaped;
+	int tmp;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
-		swaped = 0;
 		for (j = 0; j + i < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,12 +23,8 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				swaped = 1;
 				print_array(array, size);
 			}
 		}
-
-		if (swaped == 0)
-			break;
 	}
 }
