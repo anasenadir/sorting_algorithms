776f43b7e601f7c463469de5092457567aef2e50
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-21 17:58:15 +0100

    update the header file

--- sort.h
+++ sort.h
@@ -38,7 +38,11 @@ void print_array(const int *array, size_t size);
 
 /*========sorting algos========*/
 void bubble_sort(int *array, size_t size);
+
 void insertion_sort_list(listint_t **list);
+/*
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+*/
+
 #endif
