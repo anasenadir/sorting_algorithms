4058a1cb21b4f29cf7789126b05844b5f8c99544
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-21 19:34:41 +0100

    fix a mistake

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		current_minimum = i;
 		for (j = i + 1 ; j < size; j++)
