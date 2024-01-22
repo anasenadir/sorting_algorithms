092b61ca889a3d088b29f5d1324af46882dbac11
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-22 23:10:29 +0100

    include the shell_sort prototype

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
 #include <stddef.h>
 
 /**
@@ -40,5 +41,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
+/*
+void cocktail_sort_list(listint_t **list);
+*/
 #endif
