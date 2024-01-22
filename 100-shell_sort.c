652bdca2eaa2c1ec8b2923defefb7d0c684f5b5e
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-22 23:22:34 +0100

    fix betty style

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -15,30 +15,39 @@ void swap(int *ptr1, int *ptr2)
 	*ptr2 = tmp;
 }
 
+
+/**
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @array: the array to be sorted
+ * @size: the array size
+*/
 void shell_sort(int *array, size_t size)
 {
-    size_t gap, i, swaped;
+	size_t gap, i, swaped;
 
-    gap = size / 2 - 1;
+	if (array == NULL || size < 2)
+		return;
+	gap = size / 2 - 1;
 
-    swaped = 1;
-    while (swaped && gap >= 1)
-    {
-        swaped = 0;
-        for (i = 0; i < size - gap; i++)
-        {
-            if (array[i] > array[i + gap])
-            {
-                swaped = 1;
-                swap(array + i, array + i + gap);
-            }
-        }
-        print_array (array, size);
-        if (gap > 1) 
-            gap = gap / 2;
-        
-        if (swaped == 0)
-            break;
-    }
-    
+	swaped = 1;
+	while (swaped && gap >= 1)
+	{
+		swaped = 0;
+		for (i = 0; i < size - gap; i++)
+		{
+			if (array[i] > array[i + gap])
+			{
+				swaped = 1;
+				swap(array + i, array + i + gap);
+			}
+		}
+		print_array(array, size);
+		if (gap > 1)
+			gap = gap / 2;
+
+		if (swaped == 0)
+			break;
+	}
+
 }
