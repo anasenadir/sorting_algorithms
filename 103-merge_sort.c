3004ff5980b01f0e45c9b68f539595d79c491a3a
root <root@DESKTOP-PDMJJRJ.localdomain>
2024-01-23 18:06:55 +0100

    change the implementation of the array

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,47 +1,41 @@
 #include "sort.h"
 #include <stdlib.h>
-#include <limits.h>
 
 
 /**
  * merging - merging the two subarrays togother and
  * reorder thier items
  * @array: the array to be sorted
- * @start: the start position
+ * @buff: A buffer to store the sorted subarray.
+ * @start: The front index of the array.
  * @mid: the middle of the array
  * @end: the end posotion
 */
-void merging(int *array, size_t start, size_t mid, size_t end)
+void merging(int *array, int *buff, size_t start, size_t mid,
+		size_t end)
 {
-	int *result;
-	size_t i, j, k;
+	size_t i, j, k = 0;
 
-	i = start;
-	j = mid + 1;
-	k = 0;
+	printf("Merging...\n[left]: ");
+	print_array(array + start, mid - start);
 
-	result = malloc(sizeof(int) * (end - start + 1));
-	if (!result)
-		return;
+	printf("[right]: ");
+	print_array(array + mid, end - mid);
 
-	while (i <= mid && j <= end)
-	{
-		if (array[i] < array[j])
-			result[k++] = array[i++];
-		else
-			result[k++] = array[j++];
-	}
+	for (i = start, j = mid; i < mid && j < end; k++)
+		buff[k] = (array[i] < array[j]) ? array[i++] : array[j++];
+	
+	while (i < mid)
+		buff[k++] = array[i++];
 
-	while (i <= mid)
-		result[k++] = array[i++];
+	while (j < end)
+		buff[k++] = array[j++];
 
-	while (j <= end)
-		result[k++] = array[j++];
+	for (i = start, k = 0; i < end; i++)
+		array[i] = buff[k++];
 
-
-	for (i = 0; i < k; i++)
-		array[start + i] = result[i];
-	free(result);
+	printf("[Done]: ");
+	print_array(array + start, end - start);
 }
 
 
@@ -49,25 +43,22 @@ void merging(int *array, size_t start, size_t mid, size_t end)
  * merge_s - is a recursive function that divides the array
  * into halves until the base case is reached.
  * @array: the array to be sorted
+ * @buff: A buffer to store the sorted result.
  * @start: the start position
  * @end: the end posotion
- * @size: the size of the array
 */
-void merge_s(int *array, size_t start, size_t end, size_t size)
+void merge_s(int *array, int *buff, size_t start, size_t end)
 {
 	size_t mid;
 
-	if (end > start)
+	if (end - start > 1)
 	{
-		mid = (start + end) / 2;
-		merge_s(array, start, mid, size);
-		merge_s(array, mid + 1, end, size);
-
-		merging(array, start, mid, end);
-		print_array(array, size);
+		mid = (end + start) / 2;
+		merge_sort_recursive(array, buff, start, mid);
+		merge_sort_recursive(array, buff, mid, end);
+		merge_subarr(array, buff, start, mid, end);
 	}
 }
-
 
 /**
  * merge_sort - function that implements mege_sort
@@ -77,8 +68,16 @@ void merge_s(int *array, size_t start, size_t end, size_t size)
 */
 void merge_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 2)
+	int *buff;
+
+	if (array == NULL || size < 2)
 		return;
 
-	merge_s(array, 0, size - 1, size);
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	merge_s(array, buff, 0, size);
+
+	free(buff);
 }
